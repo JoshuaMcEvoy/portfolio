2cb3ed826994fd1d94297b42e1e5fad45ad3b8ea
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-02 14:35:26 +1100

    fixed nav scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,11 +12,12 @@ $(document).ready(function() {
   });
 
   // Button scroll
-  $('.js--scroll-to-plans').click(function(){
+  $('.js--scroll-to-about').click(function(){
     $('html, body').animate({scrollTop: $( '.js--section-about' ).offset().top}, 1000);
   })
-  $('.js--scroll-to-start').click(function(){
-    $('html, body').animate({scrollTop: $( '.js--section-plans' ).offset().top}, 1000);
+
+  $('.js--scroll-to-contact').click(function(){
+    $('html, body').animate({scrollTop: $( '.js--section-contact' ).offset().top}, 1000);
   });
 
   // Navigation button smooth scrolling
