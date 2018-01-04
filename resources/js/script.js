53e932634ca3d7cf43d910ae024e03a77cfafd32
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-04 16:19:30 +1100

    removed

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
     offset: '60px;'
   });
 
-  // Button scroll
   $('.js--scroll-to-about').click(function(){
     $('html, body').animate({scrollTop: $( '.js--section-about' ).offset().top}, 1000);
   })
