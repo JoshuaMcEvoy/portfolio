f1683d9ae172ae823618bb7407acd9a8ab6f0c87
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-05 12:12:25 +1100

    script

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,14 +12,7 @@ $(document).ready(function() {
     offset: '60px;'
   });
 
-  // Button scroll
-  $('.js--scroll-to-about').click(function(){
-    $('html, body').animate({scrollTop: $( '.js--section-about' ).offset().top}, 1000);
-  })
-
-  $('.js--scroll-to-contact').click(function(){
-    $('html, body').animate({scrollTop: $( '.js--section-contact' ).offset().top}, 1000);
-  });
+  
 
   // Navigation button smooth scrolling
 
