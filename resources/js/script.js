181852d5b9f683340dd520d14ac1cadcd4b16e2c
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-02-07 16:41:42 +1100

    adding button scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,14 +16,14 @@ $(document).ready(function() {
     offset: '60px;'
   });
 
-  // Button scroll
-  $('.js--scroll-to-about').click(function(){
-    $('html, body').animate({scrollTop: $( '.js--section-about' ).offset().top}, 1000);
-  })
-
-  $('.js--scroll-to-contact').click(function(){
-    $('html, body').animate({scrollTop: $( '.js--section-contact' ).offset().top}, 1000);
-  });
+  // // Button scroll
+  // $('.js--scroll-to-about').click(function(){
+  //   $('html, body').animate({scrollTop: $( '.js--section-about' ).offset().top}, 1000);
+  // })
+  //
+  // $('.js--scroll-to-contact').click(function(){
+  //   $('html, body').animate({scrollTop: $( '.js--section-contact' ).offset().top}, 1000);
+  // });
 
   // Navigation button smooth scrolling
   // Select all links with hashes
